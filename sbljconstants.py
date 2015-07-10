d889f3b84cec0077b4096e584cab34e5d16c411c
Loko Kung <lokokung@gmail.com>
2015-07-10 11:21:50 -0700

    Added setLOFreq method along with corresponding constants file. The method has been tested with a real LabJack but mock tests have yet to be written. New exception class was also created in order to handle invalid inputs into the new LO methods.

---
+++ sbljconstants.py
@@ -0,0 +1,11 @@
+"""
+    STARBURST LabJack Control Wrapper Library Constants
+    Author: Lokbondo Kung
+    Email: lkkung@caltech.edu
+"""
+
+# LO Frequencies
+LO_3_4GHZ = 0
+LO_7_5GHZ = 1
+LO_11_5GHZ = 2
+LO_15_5GHZ = 3
